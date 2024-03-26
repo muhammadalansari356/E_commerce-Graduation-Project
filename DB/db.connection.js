3317d31ab4ecb543faf6f22140eefbcf76f32b05
Muhammadalansari356 <Muhammadalansari356@gmail.com>
2024-03-26 03:52:42 +0200

    message

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose"
 
-
 export const dbConnection = async () => {
-    await mongoose.connect("mongodb+srv://hazem:hazem1907@cluster0.n1b3sux.mongodb.net/E_commerce").then(() => {
+    await mongoose.connect("mongodb+srv://muhammadalansari356:THqY6QQL2siYjWoB@cluster0.ymmfgce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
         console.log("db connect success");
     }).catch((err) => {
         console.log("db connect fail");
